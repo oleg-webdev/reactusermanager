e0009649f724191f6fe78b5bd08c09842a3e63b2
Oleg Tsybulnik <cibulnicoleg@gmail.com>
2017-08-22 21:53:23 +0300

    Multiple reducers

--- app/App.jsx
+++ app/App.jsx
@@ -1,29 +1,97 @@
-import React, {Component} from 'react';
-import { render } from 'react-dom';
-import Main from './components/Main'
-import User from './components/User'
+// import React, {Component} from 'react';
+// import { render } from 'react-dom';
+// import Main from './components/Main'
+// import User from './components/User'
+//
+// class App extends Component {
+//
+// 	state = {
+// 		username: "Oleg"
+// 	};
+//
+// 	changeUsername = (newName) => {
+// 		this.setState({
+// 			username: newName
+// 		});
+// 	}
+//
+// 	render() {
+// 		return (
+// 			<div className="container">
+// 				<Main changeUsername={this.changeUsername}/>
+// 				<User username={this.state.username}/>
+// 			</div>
+// 		)
+// 	}
+//
+// }
+//
+// render(<App/>, document.getElementById('app'));
 
-class App extends Component {
+import { createStore, combineReducers } from 'redux'
 
-	state = {
-		username: "Oleg"
-	};
 
-	changeUsername = (newName) => {
-		this.setState({
-			username: newName
-		});
+const mathReducer = (state = { result: 0, lastValues: [] }, action) => {
+
+	switch (action.type) {
+		case "ADD" :
+			state = {
+				...state,
+				result: state.result + action.payload,
+				lastValues: [ ...state.lastValues, action.payload ]
+			}
+			break;
+		case "SUBTRACT" :
+			state = {
+				...state,
+				result: state.result - action.payload,
+				lastValues: [ ...state.lastValues, action.payload ]
+			}
+			break;
 	}
 
-	render() {
-		return (
-			<div className="container">
-				<Main changeUsername={this.changeUsername}/>
-				<User username={this.state.username}/>
-			</div>
-		)
-	}
+	return state;
 
 }
 
-render(<App/>, document.getElementById('app'));
+const userReducer = (state = { name: "Oleg", age: 34 }, action) => {
+
+	switch (action.type) {
+		case "SET_NAME" :
+			state = {
+				...state,
+				name: action.payload
+			}
+			break;
+		case "SET_AGE" :
+			state = {
+				...state,
+				age: action.payload
+			}
+			break;
+	}
+
+	return state;
+
+}
+
+
+const store = createStore(combineReducers({ mathReducer, userReducer }))
+
+store.subscribe(() => {
+	console.log("Store updated!", store.getState());
+})
+
+store.dispatch({
+	type: "ADD",
+	payload: 150
+})
+store.dispatch({
+	type: "SUBTRACT",
+	payload: 20
+})
+
+store.dispatch({
+	type: "SET_AGE",
+	payload: 20
+})
--- app/__draft__/plain_redux.jsx
+++ app/__draft__/plain_redux.jsx
@@ -1,25 +1,42 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
-const initialState = {
-	result: 0,
-	lastValues: []
-}
 
-const reducer = (state = initialState, action) => {
+const mathReducer = (state = { result: 0, lastValues: [] }, action) => {
 
 	switch (action.type) {
 		case "ADD" :
 			state = {
 				...state,
 				result: state.result + action.payload,
-				lastValues: [...state.lastValues, action.payload]
+				lastValues: [ ...state.lastValues, action.payload ]
 			}
 			break;
 		case "SUBTRACT" :
 			state = {
 				...state,
 				result: state.result - action.payload,
-				lastValues: [...state.lastValues, action.payload]
+				lastValues: [ ...state.lastValues, action.payload ]
+			}
+			break;
+	}
+
+	return state;
+
+}
+
+const userReducer = (state = { name: "Oleg", age: 34 }, action) => {
+
+	switch (action.type) {
+		case "SET_NAME" :
+			state = {
+				...state,
+				name: action.payload
+			}
+			break;
+		case "SET_AGE" :
+			state = {
+				...state,
+				age: action.payload
 			}
 			break;
 	}
@@ -29,7 +46,7 @@ const reducer = (state = initialState, action) => {
 }
 
 
-const store = createStore(reducer)
+const store = createStore(combineReducers({ mathReducer, userReducer }))
 
 store.subscribe(() => {
 	console.log("Store updated!", store.getState());
@@ -41,5 +58,10 @@ store.dispatch({
 })
 store.dispatch({
 	type: "SUBTRACT",
+	payload: 20
+})
+
+store.dispatch({
+	type: "SET_AGE",
 	payload: 20
 })
