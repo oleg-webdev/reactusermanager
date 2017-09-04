fc1526365e9ebfe8cedcb1544b2aba4bc1826bcd
oleg-webdev <cibulnicoleg@gmail.com>
2017-09-04 20:50:01 +0300

    before add native branch

--- app/app-const.js
+++ app/app-const.js
@@ -1,6 +1,8 @@
 export default {
 	// siteUrl: 'http://billwebstudio.com/projects/reactusermanager/',
 	// pref: '/projects/reactusermanager',
+	// siteUrl: 'http://alicelf.com/reactusermanager/',
+	// pref: '/reactusermanager',
 	siteUrl: 'http://localhost:8080/',
 	pref: ''
 }
